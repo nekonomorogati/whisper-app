02fd37545abc38bf597cab757abeefd46ae6f2e7
nekonomorogati <nekonomorogati@gmail.com>
2026-07-16 22:33:55 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "whisper-shell-v3";
+const CACHE_NAME = "whisper-shell-v4";
 const SHELL_FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
